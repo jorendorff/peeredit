383e6499a8ea420b6f825993ee5a59c850f25eb4
Jason Orendorff <jason.orendorff@gmail.com>
2015-09-25 15:15:02 -0500

    Add RGA.tie; fix a causality-breaking bug in selecting timestamps.

--- test/test.js
+++ test/test.js
@@ -53,4 +53,46 @@ describe("RGA", () => {
     var q = new RGA(2, p.history());
     assert(q.text() === "good morning");
   });
+
+  it("sends ops to tied replicas", () => {
+    var p = new RGA(1);
+    var c = type(p, RGA.left, "hi");
+    var q = new RGA(2, p.history());
+    RGA.tie(p, q);
+
+    var c2 = type(p, c, " there");
+    assert(q.text() === "hi there");
+    type(q, c2, " kaitlin");
+    assert(p.text() === "hi there kaitlin");
+    deleteRange(q, c, c2);
+    assert(p.text() === "hi kaitlin");
+  });
+
+  it("doesn't generate bogus timestamps", () => {
+    var p = new RGA(0);
+    var q = new RGA(1);
+    RGA.tie(p, q);
+    var c = q.addRight(RGA.left, "A");
+    assert(q.text() === "A");
+    assert(p.text() === "A");
+    var d = p.addRight(RGA.left, "B");
+    assert(p.text() === "BA");
+    assert(q.text() === "BA");
+  });
+
+  it("can replicate across a chain of intermediate replicas", () => {
+    var replicas = [new RGA(0)];
+    var N = 50;
+    for (var i = 1; i < N; i++) {
+      replicas[i] = new RGA(i);
+      RGA.tie(replicas[i - 1], replicas[i]);
+    }
+    type(replicas[N-1], RGA.left, "A");
+    assert(replicas.every(r => r.text() === "A"));
+    type(replicas[0], RGA.left, "Z");
+    assert(replicas.every(r => r.text() === "ZA"));
+  });
+
+  //describe("socket.io support", () => {
+  //});
 });
