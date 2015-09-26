3b371cefbf7b84cf92731c89307fe92e808c5e3d
Jason Orendorff <jason.orendorff@gmail.com>
2015-09-25 20:25:26 -0500

    Add two new socktpair tests.

--- test/test_socketpair.js
+++ test/test_socketpair.js
@@ -34,4 +34,29 @@ describe("socketpair", () => {
     b.deliver("ack", 1);
     assert(log === "A2 B1 b2 a1");
   });
+
+  it("can deliver a message if no callback is attached", () => {
+    var pair = socketpair();
+    pair[0].emit("hello", "world");
+    pair[0].deliver("hello", "world");
+  });
+
+  it("throws if arguments to deliver and emit do not match", () => {
+    var pair = socketpair();
+    pair[0].emit("hello", {x: 3});
+    pair[0].emit("world", {x: 4});
+
+    var log = "";
+    pair[1].on("hello", obj => { log += 'h'; });
+    pair[1].on("world", obj => { log += 'w'; });
+
+    assert.throws(() => pair[0].deliver("world", {x: 4}));  // nope
+    assert(log === "");
+
+    pair[0].deliver("hello", {});
+    assert(log === "h");
+    assert.throws(() => pair[0].deliver("hello", {}));  // already delivered
+    pair[0].deliver("world", {});
+    assert(log === "hw");
+  });
 });
